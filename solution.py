16b69d329d8f71e69bb5d38b6e09c54b9c55b865
loginmarii <129678289+loginmarii@users.noreply.github.com>
2023-04-13 20:05:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,16 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1676035524  # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
+def solution(x_success: int,
+             x_cnt: int,
+             y_success: int,
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    h0 = float(x_success) / x_cnt
+    h1 = float(y_success) / y_cnt
+    if h1 > h0:
+        b = True
+    else:
+        b = False
+    return b  # Ваш ответ, True или False
